d445a19c3b21c9ebc6d0c28398851ca5221ab9c9
Cindy Nguyen <cnguyen5138@gmail.com>
2024-05-16 14:49:09 -0400

    coding portion done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 /** GENERAL FEEDBACK:
  *
  */
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target
+    const usernameResult = document.querySelector('#results-username')
+    usernameResult.textContent = `${form.username.value}`;
+    const codingLevelResult = document.querySelector('#results-coding-level')
+    codingLevelResult.textContent = `${form.codingLevel.value}`;
+    const locationResult = document.querySelector('#results-location')
+    locationResult.textContent = `${form.location.value}`;
+    const checkboxResult = document.querySelector('#results-did-like-assignment');
+    if (form.didLikeAssignment.checked) {
+        checkboxResult.textContent = "Yes"
+    } else {
+        checkboxResult.textContent = "No"
 
-const handleSubmit = () => {
+    }
+    form.reset();
 };
+document.querySelector('form').addEventListener('submit', handleSubmit)
